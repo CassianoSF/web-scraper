5d10bf89c25bd41a9f98ea314bd8c86165940cad
CassianoSF <cassianosfranco@gmail.com>
2019-07-18 11:20:12 -0300

    git init

---
+++ index.js
@@ -0,0 +1,19 @@
+var HTMLParser = require('node-html-parser').parse;
+var axios = require('axios');
+const fs = require('fs');
+
+axios({
+  url: 'http://localhost:3000/index.html',
+  method: 'get'
+}).then(req => {
+  let html = HTMLParser(req.data)
+  let els = Array.from(html.querySelectorAll(".pane-legend-item-value"));
+  let dados = []
+  els.map(el => dados.push(parseFloat(el.innerHTML.replace(/ /, ""))))
+  fs.writeFile("./dados.txt", dados.join("\n"), (err) => {
+      if(err) {
+          console.log("Deu Ruim!", err)
+      }
+      console.log("Deu Boa!");
+  }); 
+})
