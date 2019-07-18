c6fa52efdb1f8ffef32874f6c810fdc2d62b9b48
CassianoSF <cassianosfranco@gmail.com>
2019-07-18 11:30:18 -0300

    cleanup2

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
-var HTMLParser = require('node-html-parser').parse;
-var axios = require('axios');
-const fs = require('fs');
+var HTMLParser = require('node-html-parser').parse
+var axios = require('axios')
+const fs = require('fs')
 
 axios({
   url: 'http://localhost:3000/index.html',
   method: 'get'
-}).then(req => {
-  let html = HTMLParser(req.data)
-  let els = Array.from(html.querySelectorAll(".pane-legend-item-value"));
+}).then(response => {
+  let html = HTMLParser(response.data)
+  let els = Array.from(html.querySelectorAll(".pane-legend-item-value"))
   let dados = []
   els.map(el => dados.push(parseFloat(el.innerHTML.replace(/ /, ""))))
   fs.writeFile("./dados.txt", dados.join("\n"), (err) => {
       if(err) {
           console.log("Deu Ruim!", err)
       }
-      console.log("Deu Boa!");
-  }); 
+      console.log("Deu Boa!")
+  }) 
 })
